d627e2fa35f3bd956a41f812ab49911a5337ee36
Darcy Kimball <darcy@rh-ip-1876-res.ucsd.edu>
2015-11-28 17:23:55 -0800

    Wrote ctor/destructors for directory tree stuff; untested

---
+++ fstree.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fstree.h"
+#include "filesystem.h"
+
+fs_entry* new_fs_entry(char* name, file_type type, user_id user,
+  permissions perms, unsigned int size_bytes) {
+  size_t nmlen; // Length of name
+  fs_entry* new_entry; // Return value
+
+  // Sanity check: name is short enough?
+  nmlen = strlen(name);
+  if (nmlen > MAX_NM_LEN - 1) {
+    // Too long.
+    return NULL;
+  }
+  
+  // Allocate
+  new_entry = (fs_entry*)malloc(sizeof(fs_entry));
+
+  // Initialize
+  strcpy(new_entry->name, name); 
+  new_entry->type = type;
+  new_entry->user = user;
+  new_entry->perms= perms;
+  new_entry->inodes = NULL;
+
+  if (type == DIRY) {
+    new_entry->size_bytes = 0;
+    new_entry->size_blocks = 0;
+  } else {
+    new_entry->size_bytes = size_bytes;
+    new_entry->size_blocks = size_bytes/BLOCK_SIZE;
+
+    // Account for possible partially occupied block
+    if (size_bytes % BLOCK_SIZE != 0) {
+      new_entry->size_blocks++;
+    }
+  }
+
+  return new_entry;
+}
+
+fs_node* new_fs_node(fs_entry* entry, fs_node* parent) {
+  fs_node* new_node; // Return value
+
+  // Allocate/initialize
+  new_node = (fs_node*)malloc(sizeof(fs_node));
+
+  new_node->entry = entry;
+  memset(new_node->children, 0, sizeof(new_node->children));
+  new_node->num_children = 0;
+
+  // Link to parent; check for space first
+  if (parent->num_children == MAX_DIR_FILES) {
+    // No space
+    fprintf(stderr, "new_fs_node(): no space in parent's children list!");
+    return NULL;
+  }
+
+  parent->children[parent->num_children] = new_node;
+  parent->num_children++;
+
+  return new_node;
+}
+
+void delete_fs_node(fs_node** nodepp) {
+  fs_node* nodeptr = *nodepp; // For readability
+
+  // Delete each of the child nodes
+  for (unsigned int i = 0; i < nodeptr->num_children; i++) {
+    delete_fs_node(&(nodeptr->children[i]));
+  }
+
+  // Delete the file entry
+  free(nodeptr->entry);
+
+  // Delete this
+  free(nodeptr);
+
+  // Invalidate the pointer to node
+  *nodepp = NULL;
+}
--- fstree.h
+++ fstree.h
@@ -2,7 +2,7 @@
 #define FSTREE_H
 
 #define MAX_DIR_FILES 64 // This was arbitrarily chosen
-#define FILE_NM_LEN 128 // Max filename length
+#define MAX_NM_LEN 128 // Max filename length
 
 //
 // The filesystem directory, as represented as nodes with multiple children.
@@ -32,37 +32,50 @@ typedef struct {
   unsigned char write: 1; // 1 if has write permission
 } permissions;
 
+// An index node; to be used in singly linked list
+typedef struct index_node {
+  unsigned int index; // Index of block on disk
+  unsigned int offset; // Offset into the block for the end of the used portion
+  struct index_node* next; // Pointer to next on list; NULL if last
+} index_node;
+
 // File/directory entry
 typedef struct {
-  char name[FILE_NM_LEN]; // Name of the file/dir
+  char name[MAX_NM_LEN]; // Name of the file/dir
   file_type type; // File type
   user_id user; // User ID of owner 
   permissions perms; // Permissions  
   unsigned int size_blocks; // Number of blocks used
   unsigned int size_bytes; // Number of bytes used
+  index_node* inodes; // List of indices of blocks used for this file
 } fs_entry;
+
 
 // A node representing something in the filesystem
 typedef struct fs_node {
-  fs_entry entry; // The entry associated with this node
+  fs_entry* entry; // Pointer to entry associated with this node
   struct fs_node* children[MAX_DIR_FILES]; // List of children; NULL for regular files
   unsigned int num_children; // Number of children
 } fs_node;
 
 //
-// Constructors
+// Constructors/destructors. These functions only set up objects pertaining
+// to the directory structure; they do *not* actually modify the 'disk' in
+// any way. The callers of these functions must do that.
 //
 
-// Construct a new filesystem entry; returns NULL if unsuccessful
-// This will create a new regular file if 'type' is not DIRE, with size
-// size_bytes
+// Construct a new filesystem entry; returns NULL if unsuccessful. Caller
+// must make sure the file is actually created, and the inodes are initialized.
 fs_entry* new_fs_entry(char* name, file_type type, user_id user,
   permissions perms, unsigned int size_bytes);
 
-// Construct a new filesystem node with no children; returns NULL if unsuccessful
+// Construct a new filesystem node with no children; returns NULL if
+// unsuccessful. Links the new node into the parent's children list.
 fs_node* new_fs_node(fs_entry* entry, fs_node* parent);
 
-// Delete a filesystem node; deletes all its associated children
+// Delete a filesystem node; deletes all its associated children. Again, the
+// caller must make sure that the actual files on disk are deleted (blocks
+// freed, etc.).
 void delete_fs_node(fs_node** nodepp);
 
 #endif // FSTREE_H
